7d182497942de38c5e6b8a99d65278bd72c977a9
fxdmhtt <48937737+fxdmhtt@users.noreply.github.com>
2025-08-23 15:20:58 +0800

    reset Commit 083f395
    https://github.com/open-vela/apps_graphics_lvgl/commit/083f3950ebf3350a7b7ce3b52affa25e07497f2c

--- src/core/lv_obj.c
+++ src/core/lv_obj.c
@@ -191,9 +191,9 @@ void lv_obj_add_state(lv_obj_t * obj, lv_state_t state)
     lv_state_t new_state = obj->state | state;
     if(obj->state != new_state) {
 
-        if(new_state & LV_STATE_DISABLED) {
-            lv_indev_reset(NULL, obj);
-        }
+        // if(new_state & LV_STATE_DISABLED) {
+        //     lv_indev_reset(NULL, obj);
+        // }
 
         update_obj_state(obj, new_state);
     }
